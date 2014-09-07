256f318ec3bd5c6629c201dca7ab7ae7535425e0
toxinu <toxinu@gmail.com>
2014-09-07 23:13:12 +0200

    Add Transaction types_map

--- bitstamp/resources/transaction.py
+++ bitstamp/resources/transaction.py
@@ -2,10 +2,14 @@ from __future__ import absolute_import
 from __future__ import unicode_literals
 
 from . import Resource
-from . import TimestampResource
 
 
-class Transaction(Resource, TimestampResource):
+class Transaction(Resource):
     def __init__(self, **kwargs):
-        self._types_map = {}
+        self._types_map = {
+            'date': int,
+            'tid': int,
+            'price': float,
+            'amount': float
+        }
         self._type_data(kwargs)
