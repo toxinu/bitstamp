e55a2aecc8cf78a95bfde9333167fc1f163e2258
toxinu <toxinu@gmail.com>
2014-09-07 23:13:29 +0200

    Add bitstamp to sys.path in tests __init__

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,6 @@
 from __future__ import unicode_literals
 import os
+import sys
 
 FIXTURES_PATH = os.path.join(os.path.dirname(__file__), 'fixtures')
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
